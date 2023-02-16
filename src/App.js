87ac8a1c32eb822b943a3100ff2f6c4b5efcec5f
reza narimani <namireza.com@gmail.com>
2023-02-16 00:26:48 -0800

    added pages and shared components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+//styled => creates a component witch contains css styles
+import {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Private,
+  Products,
+  SingleProduct,
+} from "./pages/EntryPoint";
+import { Sidebar, Navbar, Footer } from "./components/EntryPoint";
 function App() {
   return (
-    <>
-    app component
-    </>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/EntryPoint.js
@@ -0,0 +1,23 @@
+//main entry point to export all pages from this folder
+
+import Home from "./HomePage";
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Private from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Private,
+  Products,
+  SingleProduct,
+};
