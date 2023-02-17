ee4cc800f856678f4c3f4b09b943e241659821a9
reza narimani <namireza.com@gmail.com>
2023-02-16 21:10:14 -0800

    shared component:navbar completed

---
+++ src/components/CartLogButtons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartLogButtons = () => {
+  return (
+    <div>CartLogButtons</div>
+  )
+}
+
+export default CartLogButtons
---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+import React from "react";
+//an array for the links used in navbar
+export const links = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "products",
+    url: "/products",
+  },
+];
